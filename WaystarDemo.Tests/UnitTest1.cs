9b1561959c9058620042bfc3485edbaaf10b5e38
Bree Latimer <77258021+BreeLatimer@users.noreply.github.com>
2026-03-04 12:08:58 -0500

    Real unit test written

--- WaystarDemo.Tests/UnitTest1.cs
+++ WaystarDemo.Tests/UnitTest1.cs
@@ -1,12 +1,20 @@
 using Xunit;
+using WaystarDemo.Controllers;
 
 namespace WaystarDemo.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void WeatherForecast_Returns_Five_Results()
     {
+	// Arrange
+	var controller = new WeatherForecastController(null);
 
+	// Act
+	var result = controller.Get();
+
+	// Assert
+	Assert.Equal(5, result.Count());
     }
 }
